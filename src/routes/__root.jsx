c914b04b319a91190f8d484bd4bca0addd67bd62
MozaSuryaPutra <mozasuryaputra@gmail.com>
2024-11-22 09:41:02 +0700

    +component navbar

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -5,11 +5,12 @@ import Container from "react-bootstrap/Container";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
-
+import NavBar from "../components/NavBar";
 export const Route = createRootRoute({
   component: () => {
     return (
       <>
+        <NavBar />
         <Container fluid={true} className="p-0">
           <Outlet />
         </Container>
